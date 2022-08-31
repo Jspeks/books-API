cc0048578b20880b03107639a7cede30cbb8192d
unknown <jessespeks@hotmail.com>
2022-08-31 17:45:48 -0400

    Can't get books page to populate

--- models/books.js
+++ models/books.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 let bookSchema = new mongoose.Schema(
     {
-    "id":{},
     "title": {type: String},
     "description": {type: String},
     "year": {type: Number},
